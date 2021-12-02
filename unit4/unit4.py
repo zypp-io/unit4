9804c52010cbf58215884964f776bc3697116778
Melvin Folkers <melvin@yellowstacks.nl>
2021-12-02 02:23:34 +0100

    added new modules

--- unit4/unit4.py
+++ unit4/unit4.py
@@ -1,56 +1,114 @@
-import json
-import os
+import pandas as pd
 
-import requests
+from unit4.core import Base
 
 
-class Unit4:
+class Unit4(Base):
+    """
+    Class for importing datasets using the Unit 4 API. For a full list of modules check the unit 4 documentation:
+    https://api.multivers.nl/V221/Help. The request_data function returns a json object for the module and in the
+    functions in this class the datasets are transformed into dataframes.
+    """
+
     def __init__(self):
-        self.api_version = "V221"
-        self.base_url = f"https://api.boekhoudgemak.nl/{self.api_version}"
-        self.client_id = os.environ.get("UNIT4_CLIENT_ID")
-        self.client_secret = os.environ.get("UNIT4_CLIENT_SECRET")
-        self.refresh_token = os.environ.get("UNIT4_REFRESH_TOKEN")
-        self.access_token = self.request_access_token()
-        self.scope = ""
+        super().__init__()
 
-    def request_access_token(self):
-        endpoint = "/Oauth/Token"
-        url = self.base_url + endpoint
-        response = requests.post(
-            url,
-            data={
-                "refresh_token": self.refresh_token,
-                "client_id": self.client_id,
-                "client_secret": self.client_secret,
-                "grant_type": "refresh_token",
-            },
-        )
-        data = json.loads(response.text)
-        access_token = data.get("access_token", "no access token")
-
-        return access_token
-
-    def request_data(self, endpoint: str):
+    def accounts(self, database: str, fiscal_year: int) -> pd.DataFrame:
         """
 
         Parameters
         ----------
-        endpoint: str
-            url endpoint (suffix) of the data that needs to be extracted
+        database: str
+            name of the administration.
+        fiscal_year: int
+            fiscal year for request.
+
+        Returns
+        -------
+        df: pd.DataFrame
+            dataset containing account names and values.
+        """
+
+        data = self.request_data(endpoint=f"api/{database}/AccountNVL/All/{fiscal_year}")
+        df = pd.DataFrame(data)
+        df["administration"] = database
+        df["fiscal_year"] = str(fiscal_year)
+
+        return df
+
+    def trial_balance(self, database: str, fiscal_year: int) -> pd.DataFrame:
+        """
+
+        Parameters
+        ----------
+        database: str
+            name of the administration.
+        fiscal_year: int
+            fiscal year for request.
+
+        Returns
+        -------
+        df: pd.DataFrame
+            dataset containing account names and values.
+        """
+
+        data = self.request_data(endpoint=f"api/{database}/AccountPeriodTotalInfoList/{fiscal_year}")
+        df = pd.DataFrame(data)
+        df["administration"] = database
+        df["fiscal_year"] = str(fiscal_year)
+
+        return df
+
+    def account_info(self, database: str, account_id: str):
+        """
+        Function for getting account information. In this information is the account category, which is relevant for
+        getting the appropriate label in reports.
+
+        Parameters
+        ----------
+        database: str
+            name of the administration.
+        account_id: str
+            account id for request.
 
         Returns
         -------
 
         """
-        url = self.base_url + endpoint
-        response = requests.get(
-            url, headers={"Accept": "application/json", "Authorization": f"Bearer {self.access_token}"}
-        )
 
-        data = json.loads(response.text)
+        data = self.request_data(endpoint=f"api/{database}/AccountInfo/{account_id}")
+        df = pd.DataFrame(data, index=[0])
+        df["administration"] = database
 
-        return data
+        return df
 
-    def run(self):
-        pass
+    def account_categories(self, database: str):
+        """
+        Function for getting account categories. Thesse categories entail the mapping of accounts in the trial balances
+        and ultimately the annual reports.
+
+        Returns
+        -------
+        df: pd.DataFrame
+            dataset containing Account category names and values.
+        """
+
+        data = pd.DataFrame(self.request_data(endpoint=f"api/{database}/AccountCategoryNVL"))
+        df = pd.DataFrame(data)
+        df["administration"] = database
+
+        return df
+
+    def administrations(self) -> pd.DataFrame:
+        """
+
+        Returns
+        -------
+        df: pd.DataFrame
+            dataset containing administration names and values.
+        """
+
+        data = self.request_data(endpoint="api/AdministrationNVL")
+        df = pd.DataFrame(data)
+
+        return df
